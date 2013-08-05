68dc6cc5434fd544aa93ecde4845cc96fd0b85cf
Quico Moya <me@qmoya.com>
2013-08-05 10:43:29 +0200

    Validate email

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
+setTimeout(function () {
+	window.scrollTo(0, 1);
+}, 1000);
 
+document.addEventListener('touchmove', function () {
+	event.preventDefault();
+});
+
+var emailTextField = document.getElementById('email'),
+	emailIsValid = function (email) { 
+	    var re = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	    return re.test(email);
+	},
+	signUpButton = document.getElementById('sign-up'),
+
+	enableSignUpButton = function () {
+		console.log('enable');
+		signUpButton.disabled = 'false';
+		signUpButton.style.color = 'red';
+	},
+
+	disableSignUpButton = function () {
+		console.log('disable');
+		signUpButton.disabled = 'true';
+		signUpButton.style.color = 'darkgray';
+	};
+
+
+emailTextField.addEventListener('keydown', function (event) {
+	setTimeout(function() {
+		if (emailIsValid(event.srcElement.value)) {
+			enableSignUpButton();
+		} else {
+			disableSignUpButton();
+		}
+	}, 100);
+});
+
+emailTextField.addEventListener('blur', function (event) {
+	console.log('blur');
+	window.scrollTo(0, 1);
+});
