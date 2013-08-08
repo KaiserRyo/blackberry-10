244e721d49be2889ee2493f6d33daa4b247a2f9f
Quico Moya <me@qmoya.com>
2013-08-08 16:26:00 +0200

    Make all fields mandatory

--- js/main.js
+++ js/main.js
@@ -1,41 +1,53 @@
+/*jslint browser: true, devel: true*/
+
 setTimeout(function () {
-	window.scrollTo(0, 1);
+    'use strict';
+    window.scrollTo(0, 1);
 }, 1000);
 
 document.addEventListener('touchmove', function () {
-	event.preventDefault();
+    'use strict';
+    event.preventDefault();
 });
 
 var emailTextField = document.getElementById('email'),
-	firstNameTextField = document.getElementById('first-name'),
-	lastNameTextField = document.getElementById('last-name'),
-	emailIsValid = function (email) { 
-	    var re = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	    return re.test(email);
-	},
-	signUpButton = document.getElementById('sign-up'),
+    firstNameTextField = document.getElementById('first-name'),
+    lastNameTextField = document.getElementById('last-name'),
+    emailIsValid = function (email) {
+        'use strict';
+        var re = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return re.test(email);
+    },
+    signUpButton = document.getElementById('sign-up'),
 
-	enableSignUpButton = function () {
-		signUpButton.disabled = false;
-		signUpButton.style.color = 'red';
-	},
+    enableSignUpButton = function () {
+        'use strict';
+        signUpButton.disabled = false;
+        signUpButton.style.color = 'red';
+    },
 
-	disableSignUpButton = function () {
-		signUpButton.disabled = true;
-		signUpButton.style.color = 'darkgray';
-	};
+    disableSignUpButton = function () {
+        'use strict';
+        signUpButton.disabled = true;
+        signUpButton.style.color = 'darkgray';
+    },
 
+    keydown = function () {
+        'use strict';
+        setTimeout(function() {
+            if (emailIsValid(emailTextField.value) && firstNameTextField.value !== "" && lastNameTextField.value !== "") {
+                enableSignUpButton();
+            } else {
+                disableSignUpButton();
+            }
+        }, 100);
+    };
 
-emailTextField.addEventListener('keydown', function (event) {
-	setTimeout(function() {
-		if (emailIsValid(event.srcElement.value)) {
-			enableSignUpButton();
-		} else {
-			disableSignUpButton();
-		}
-	}, 100);
-});
+emailTextField.addEventListener('keydown', keydown);
+firstNameTextField.addEventListener('keydown', keydown);
+lastNameTextField.addEventListener('keydown', keydown);
 
-emailTextField.addEventListener('blur', function (event) {
-	window.scrollTo(0, 1);
+emailTextField.addEventListener('blur', function () {
+    'use strict';
+    window.scrollTo(0, 1);
 });
